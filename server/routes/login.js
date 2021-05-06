430a98fd0df0c24f508d7becedd8fa8e54fe8170
Miro <miroruopsa@gmail.com>
2021-05-06 22:40:05 +0300

    login post method works

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,14 +5,19 @@ const route = express.Router();
 const HTML_ROOT = `${__dirname}/../build/login/html`;
 
 route.use(bodyParser.urlencoded({ extended: true }));
+route.use(express.json());
 
 const isValidAuth = function isValidAuth(req, res, next) {
 	console.log(req.body);
 	next();
 };
 
-route.get("/", isValidAuth, (req,res) => {
+route.get("/", (req,res) => {
 	res.sendFile("login.html", { root: HTML_ROOT});
+});
+
+route.post("/", isValidAuth, (req,res) => {
+	
 });
 
 module.exports = route;
